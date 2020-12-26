a6643c30ccae03196a5e5fe703ac26d320189227
Zord <zord.bk@gmail.com>
2020-12-26 22:48:30 +0100

    Improved help

--- src/tools/Help.jsx
+++ src/tools/Help.jsx
@@ -11,7 +11,7 @@ const Key = displayName(
     margin: 0 0.1rem 0 0.2rem;
     background: #999;
     box-shadow: -0.08rem 0.08rem 0 0.08rem #777;
-    color: #444;
+    color: #333;
     min-width: 1rem;
     height: 1rem;
     display: inline-block;
@@ -26,6 +26,15 @@ const Heading = displayName(
     line-height: 1.5;
     font-size: 1.3rem;
     font-weight: bold;
+  `
+);
+
+const Count = displayName(
+  "Count",
+  styled.span`
+    font-size: 0.7rem;
+    font-weight: 400;
+    opacity: 0.2;
   `
 );
 
@@ -54,14 +63,15 @@ const ToolName = displayName(
 const ToolDesc = displayName(
   "ToolDesc",
   styled.span`
-    opacity: 0.6;
+    opacity: 0.5;
   `
 );
 
 const Footer = displayName(
   "Footer",
   styled.p`
-    opacity: 0.1;
+    font-size: 0.7rem;
+    opacity: 0.2;
   `
 );
 
@@ -77,14 +87,17 @@ const Help = ({ tools, onSelectTool = noop }) => {
         Press <Key>esc</Key> or <Key>⌘</Key>
         <Key>F</Key> to search for a tool.
       </p>
-      <Heading>Tools</Heading>
+      <Heading>
+        <span>Tools </span>
+        <Count>{sortedTools.length}</Count>
+      </Heading>
       <ToolList>
         {sortedTools.map((tool) => (
           <ToolItem key={tool.name}>
             <ToolName onClick={() => onSelectTool(tool.name)}>
               {tool.name}
             </ToolName>
-            <ToolDesc> - {tool.description}</ToolDesc>
+            <ToolDesc> — {tool.description}</ToolDesc>
           </ToolItem>
         ))}
       </ToolList>
